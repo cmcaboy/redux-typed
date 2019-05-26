d3defec3357b34b0d329fd4a95d0c5b99b4be75e
Cory McAboy <cory.mcaboy@gmail.com>
2019-05-26 08:07:03 -0400

    cleaned up HomePage

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,22 +1,24 @@
 import React from "react";
 import { connect } from "react-redux";
 import { startEditExpense, startRemoveExpense } from "../actions/expenses";
+import expenses from "../reducers/expenses";
 
 export class EditExpensePage extends React.Component {
-  onSubmit = expense => {
+  onEdit = expense => {
     this.props.startEditExpense(this.props.expense.id, expense);
   };
   onRemove = () => {
     this.props.startRemoveExpense({ id: this.props.expense.id });
   };
   render() {
+    const { expenses } = this.props;
     return (
       <div>
         <h1>Expense Page</h1>
         <div>
-          <ExpenseForm expense={this.props.expense} onSubmit={this.onSubmit} />
+          <p>{expenses}</p>
           <button onClick={this.onRemove}>Remove Expense</button>
-          <button onClick={this.onAdd}>Edit Expense</button>
+          <button onClick={this.onEdit}>Edit Expense</button>
         </div>
       </div>
     );
